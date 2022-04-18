d203c0c4ebb78f42fff7344d751105dec121fbdf
Stylianos Dimitriadis <sdimitriadis17@gmail.com>
2022-04-18 14:16:24 -0400

    See previous commit message

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/weather/:latlon', async (request, response) => {
     const lat = latlon[0];
     const lon = latlon[1];
     const api_key = '94a83aa608751b129542eb8e4829206c';
-    const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&appid=${api_key}`;
+    const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${api_key}`;
     const weather_response = await fetch(api_url);
     const json = await weather_response.json();
     response.json(json);
